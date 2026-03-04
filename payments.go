cf7531d9975739534d01a24e013e41ae99bc3a61
Andrew Mellen <andrew_mellen@icloud.com>
2026-03-04 11:48:29 -0500

    add new all to query payments (#3)

--- payments.go
+++ payments.go
@@ -2,6 +2,7 @@ package payments
 
 import (
 	"context"
+	"iter"
 	"time"
 
 	"github.com/Rhymond/go-money"
@@ -40,4 +41,42 @@ type CheckoutManager interface {
 	// Note that this can still return metadata even if err != nil, in the event that the
 	// checkout was expired, since the caller may still want to know checkout information.
 	ConfirmCheckout(ctx context.Context, payload []byte, signature string) (map[string]string, error)
+}
+
+type Address struct {
+	City       string
+	Country    string
+	Line1      string
+	Line2      string
+	PostalCode string
+	State      string
+}
+
+type BillingDetails struct {
+	Name    string
+	Email   string
+	Phone   string
+	Address *Address
+}
+
+type Payment struct {
+	ID                string
+	Amount            *money.Money
+	Created           time.Time
+	Status            string
+	Metadata          map[string]string
+	Description       string
+	BillingDetails    *BillingDetails
+	CheckoutSessionID string
+}
+
+type ChargeListParams struct {
+	CreatedAfter   *time.Time
+	CreatedBefore  *time.Time
+	Status         string
+	MetadataFilter map[string]string
+}
+
+type PaymentQuerier interface {
+	ListCharges(ctx context.Context, params ChargeListParams) iter.Seq2[Payment, error]
 }
